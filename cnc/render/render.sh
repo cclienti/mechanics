b51023e0a8493e36bdeeaca9919eb366e8468e59
Christophe Clienti <cclienti@users.noreply.github.com>
2023-11-01 10:48:53 +0100

    Improved cnc rendering script to move also the z-axis

--- cnc/render/render.sh
+++ cnc/render/render.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-parallel -j 28 ./animate.sh ::: {80..800}
+parallel -j 28 ./animate.sh ::: {80..700}
 ffmpeg -framerate 30 -pattern_type glob -i 'animated/*.png' -c:v libx264 -pix_fmt yuv420p plasma.mp4
