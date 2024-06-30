d4fce149af54c661fd3e88d5740593296a9ee6d1
Christophe Clienti <cclienti@users.noreply.github.com>
2024-06-30 22:31:00 +0200

    [cross-table][fw] Switched to the new FatFS sdcard libray

--- cross-table/firmware/src/sdcard.cpp
+++ cross-table/firmware/src/sdcard.cpp
@@ -8,14 +8,18 @@
 
 void test_sd_card()
 {
-    sd_card_t *pSD = sd_get_by_num(0);
-    f_mount(&pSD->fatfs, pSD->pcName, 1);
+    FATFS fs;
+    f_mount(&fs, "", 1);
+
     FIL fil;
     f_open(&fil, "file1.txt", FA_READ);
+
     char buffer[64];
     memset(buffer, 0, sizeof(buffer));
+
     UINT br;
     f_read(&fil, buffer, sizeof(buffer)-1, &br);
     buffer[br] = 0;
+
     printf("%s", buffer);
 }
--- cross-table/firmware/src/sdcard_config.cpp
+++ cross-table/firmware/src/sdcard_config.cpp
@@ -1,47 +1,35 @@
-#include "hardware/gpio.h"
-#include "sd_card.h"
 #include "hw_config.h"
 
+/* Configuration of RP2040 hardware SPI object */
 static spi_t spi = {
-    .hw_inst                  = spi0,
-    .miso_gpio                = 4,
+    .hw_inst                  = spi0,  // RP2040 SPI component
+    .miso_gpio                = 4     ,// GPIO number (not Pico pin number)
     .mosi_gpio                = 3,
     .sck_gpio                 = 2,
-    .baud_rate                = 125 * 1000,
+    .baud_rate                = 12 * 1000 * 1000,   // Actual frequency: 10416666.
     .set_drive_strength       = false,
     .mosi_gpio_drive_strength = GPIO_DRIVE_STRENGTH_4MA,
     .sck_gpio_drive_strength  = GPIO_DRIVE_STRENGTH_4MA
 };
 
-static sd_card_t sd_card = {
-    .pcName          = "0:",  // Name used to mount device
-    .spi             = &spi,  // Pointer to the SPI driving this card
-    .ss_gpio         = 5,     // The SPI slave select GPIO for this SD card
-    .use_card_detect = false
+/* SPI Interface */
+static sd_spi_if_t spi_if = {
+    .spi     = &spi,   // Pointer to the SPI driving this card
+    .ss_gpio = 5       // The SPI slave select GPIO for this SD card
 };
 
-size_t spi_get_num()
-{
-    return 1;
-}
+/* Configuration of the SD Card socket object */
+static sd_card_t sd_card = {
+    .type            = SD_IF_SPI,
+    .spi_if_p        = &spi_if,  // Pointer to the SPI interface driving this card
+    .use_card_detect = false,
 
-spi_t *spi_get_by_num(size_t num)
-{
-    if (num < sd_get_num()) {
-        return &spi;
-    } else {
-        return NULL;
-    }
-}
+};
 
-size_t sd_get_num()
-{
-    return 1;
-}
+size_t sd_get_num() { return 1; }
 
-sd_card_t *sd_get_by_num(size_t num)
-{
-    if (num < sd_get_num()) {
+sd_card_t *sd_get_by_num(size_t num) {
+    if (0 == num) {
         return &sd_card;
     } else {
         return NULL;
