14a8ca9700be754fea52dc8f3fde3888ea8f0411
Christophe Clienti <cclienti@users.noreply.github.com>
2024-07-09 08:12:59 +0200

    [cross-table][fw] Added the support of a menu generic dialog in LCDMenu

--- cross-table/firmware/src/lcd_menu.cpp
+++ cross-table/firmware/src/lcd_menu.cpp
@@ -15,8 +15,6 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include "cross-table/lcd_menu.hpp"
-#include "cross-table/lcd_display.hpp"
-#include "cross-table/switch.hpp"
 
 #include <string>
 #include <cstdint>
@@ -50,24 +48,22 @@ LCDMenu::LCDMenu():
 	m_lcd_display.home();
 
 	// Write custom chars for splash
-	std::uint8_t char_1[8] = {0x07, 0x04, 0x04, 0x1C, 0x1C, 0x04, 0x04, 0x07}; // splash -[
-	std::uint8_t char_2[8] = {0x1C, 0x04, 0x04, 0x07, 0x07, 0x04, 0x04, 0x1C}; // splash ]-
-	std::uint8_t char_3[8] = {0x00, 0x00, 0x00, 0x1F, 0x1F, 0x00, 0x00, 0x00}; // Thick dot
-	std::uint8_t char_4[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x1F, 0x00}; // Thick underscore
-	std::uint8_t char_5[8] = {0x03, 0x06, 0x0C, 0x18, 0x18, 0x0C, 0x06, 0x03}; // Title <
-	std::uint8_t char_6[8] = {0x18, 0x0C, 0x06, 0x03, 0x03, 0x06, 0x0C, 0x18}; // Title >
+    // Example for MenuUp
+    //   1
+    //   0 8 4 2 1
+    //   . . . . .  0x00
+    //   . . . . .  0x00
+    //   . . * . .  0x04
+    //   . * * * .  0x0E
+    //   * * * * *  0x1F
+    //   . . . . .  0x00
+    //   . . . . .  0x00
+    //   . . . . .  0x00
+
+	std::uint8_t char_1[8] = {0x00, 0x00, 0x04, 0x0E, 0x1F, 0x00, 0x00, 0x00}; // MenuUp
+	std::uint8_t char_2[8] = {0x00, 0x00, 0x1F, 0x0E, 0x04, 0x00, 0x00, 0x00}; // MenuDown
 	m_lcd_display.set_char(1, char_1);
 	m_lcd_display.set_char(2, char_2);
-	m_lcd_display.set_char(3, char_3);
-	m_lcd_display.set_char(4, char_4);
-	m_lcd_display.set_char(5, char_5);
-	m_lcd_display.set_char(6, char_6);
-}
-
-
-void LCDMenu::register_menu(const std::string &title, EntryDisplayCallback cb)
-{
-    m_menu_entries.emplace_back(title, cb);
 }
 
 
@@ -109,6 +105,44 @@ void LCDMenu::splash(const std::string &text)
 }
 
 
+void LCDMenu::print_variant(const EntryDialogItem &item)
+{
+    if (std::holds_alternative<bool>(item)) {
+        if (std::get<bool>(item)) {
+            m_lcd_display.printf("    ON");
+        } else {
+            m_lcd_display.printf("   OFF");
+        }
+    }
+    else if (std::holds_alternative<int>(item)) {
+        m_lcd_display.printf("%6d", std::get<int>(item));
+    }
+    else if (std::holds_alternative<float>(item)) {
+        m_lcd_display.printf("%6.01f", std::get<float>(item));
+    }
+}
+
+void LCDMenu::update_variant(int incr, int decr, EntryDialogItem &item)
+{
+    if (std::holds_alternative<bool>(item)) {
+        if (incr > 0) {
+            item = true;
+        }
+        if (decr > 0) {
+            item = false;
+        }
+    }
+    else if (std::holds_alternative<int>(item)) {
+        int v = std::get<int>(item);
+        item = v + incr - decr;
+    }
+    else if (std::holds_alternative<float>(item)) {
+        float v = std::get<float>(item);
+        v = (v * 10.0f + incr - decr) * 0.1f;
+        item = v;
+    }
+}
+
 void LCDMenu::refresh_menu()
 {
     char buf[256];
@@ -117,12 +151,68 @@ void LCDMenu::refresh_menu()
 		return;
 	}
 
-	const auto &entry = m_menu_entries[m_current_entry_id];
+    if (m_buttons.menu.is_released()) {
+        switch_entry();
+    }
 
-	m_lcd_display.set_pos(0, 0);
-	m_lcd_display.print(format_info(entry.first, m_lcd_display.get_num_cols(), '-'));
+    auto &entry = m_menu_entries[m_current_entry_id];
 
-	m_lcd_display.set_pos(1, 0);
-    entry.second(buf);
-	m_lcd_display.print(buf);
+    /// Print the menu title and prepare the cursor position.
+    m_lcd_display.set_pos(0, 0);
+    m_lcd_display.print(format_info(entry.name, m_lcd_display.get_num_cols(), '-'));
+
+    // Dispatch to the right menu handler.
+    EntryCallback cb_variant = entry.cb;
+    if (std::holds_alternative<EntryDisplayCallback>(cb_variant)) {
+        auto cb = std::get<EntryDisplayCallback>(cb_variant);
+        cb(buf, m_buttons);
+        m_lcd_display.set_pos(1, 0);
+        m_lcd_display.print(buf);
+    }
+    else if (std::holds_alternative<EntryDialogCallback>(cb_variant)) {
+        if (entry.dialog_items.empty()) {
+            return;
+        }
+        // Select the right item depending on the state
+        auto &item = entry.dialog_items[entry.state];
+
+        // Display current item
+        m_lcd_display.set_pos(1, 9);
+        m_lcd_display.print("\1");
+        m_lcd_display.set_pos(2, 0);
+        m_lcd_display.print(item.first.c_str()); // TODO Truncate string if needed.
+        m_lcd_display.set_pos(2, 14);
+        print_variant(item.second);
+        m_lcd_display.set_pos(3, 9);
+        m_lcd_display.print("\2");
+
+        // Switch between dialog item
+        if (m_buttons.y_plus.is_released()) {
+            if (entry.state == 0) {
+                entry.state = entry.dialog_items.size()-1;
+            }
+            else {
+                entry.state--;
+            }
+        }
+        if (m_buttons.y_minus.is_released()) {
+            if (entry.state == entry.dialog_items.size()-1) {
+                entry.state = 0;
+            }
+            else {
+                entry.state++;
+            }
+        }
+
+        // Update current value
+        update_variant(m_buttons.x_plus.is_pressed(), m_buttons.x_minus.is_pressed(), item.second);
+
+        // Handle callback
+        auto ok_released = m_buttons.ok.is_released();
+        auto reset_released = m_buttons.ok.is_released();
+        if (ok_released || reset_released) {
+            auto cb = std::get<EntryDialogCallback>(cb_variant);
+            cb(ok_released, reset_released, entry.dialog_items);
+        }
+    }
 }
